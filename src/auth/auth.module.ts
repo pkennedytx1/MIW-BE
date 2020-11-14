6d3f356f181c892f7937fc6006498e7e10a97b47
Patrick Kennedy <pkennedytx1@gmail.com>
2020-11-14 12:47:18 -0600

    added auth and jwt token logic

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
-// import { AuthResolver } from './auth.resolver';
+import { AuthResolver } from './auth.resolver';
 import { PrismaModule } from '../prisma/prisma.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
-// import { JwtStrategy } from './jwt.strategy';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
@@ -19,7 +19,6 @@ import { JwtModule } from '@nestjs/jwt';
       },
     }),
   ],
-  // AuthResolver, JwtStrategy
-  providers: [AuthService],
+  providers: [AuthService, AuthResolver, JwtStrategy],
 })
 export class AuthModule {}
--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -1,61 +1,61 @@
-// import * as bcryptjs from 'bcryptjs';
-// import { Response } from 'express';
-// import { Args, Mutation, Resolver } from '@nestjs/graphql';
-// import { LoginInput } from '../graphql.schema.generated';
-// import { ResGql } from '../shared/decorators/decorators';
-// import { JwtService } from '@nestjs/jwt';
-// import { PrismaService } from '../prisma/prisma.service';
-// import { SignUpInputDto } from './sign-up-input.dto';
+import * as bcryptjs from 'bcryptjs';
+import { Response } from 'express';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { LoginInput } from '../graphql.schema.generated';
+import { ResGql } from '../shared/decorators/decorators';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from '../prisma/prisma.service';
+import { SignUpInputDto } from './sign-up-input.dto';
 
-// @Resolver('Auth')
-// export class AuthResolver {
-//   constructor(
-//     private readonly jwt: JwtService,
-//     private readonly prisma: PrismaService,
-//   ) {}
+@Resolver('Auth')
+export class AuthResolver {
+  constructor(
+    private readonly jwt: JwtService,
+    private readonly prisma: PrismaService,
+  ) {}
 
-//   @Mutation()
-//   async login(
-//     @Args('loginInput') { email, password }: LoginInput,
-//     @ResGql() res: Response,
-//   ) {
-//     const user = await this.prisma.client.user({ email });
-//     if (!user) {
-//       throw Error('Email or password incorrect');
-//     }
+  @Mutation()
+  async login(
+    @Args('loginInput') { email, password }: LoginInput,
+    @ResGql() res: Response,
+  ) {
+    const user = await this.prisma.client.user({ email });
+    if (!user) {
+      throw Error('Email or password incorrect');
+    }
 
-//     const valid = await bcryptjs.compare(password, user.password);
-//     if (!valid) {
-//       throw Error('Email or password incorrect');
-//     }
+    const valid = await bcryptjs.compare(password, user.password);
+    if (!valid) {
+      throw Error('Email or password incorrect');
+    }
 
-//     const jwt = this.jwt.sign({ id: user.id });
-//     res.cookie('token', jwt, { httpOnly: true });
+    const jwt = this.jwt.sign({ id: user.id });
+    res.cookie('token', jwt, { httpOnly: true });
 
-//     return user;
-//   }
+    return user;
+  }
 
-//   @Mutation()
-//   async signup(
-//     @Args('signUpInput') signUpInputDto: SignUpInputDto,
-//     @ResGql() res: Response,
-//   ) {
-//     const emailExists = await this.prisma.client.$exists.user({
-//       email: signUpInputDto.email,
-//     });
-//     if (emailExists) {
-//       throw Error('Email is already in use');
-//     }
-//     const password = await bcryptjs.hash(signUpInputDto.password, 10);
+  @Mutation()
+  async signup(
+    @Args('signUpInput') signUpInputDto: SignUpInputDto,
+    @ResGql() res: Response,
+  ) {
+    const emailExists = await this.prisma.client.$exists.user({
+      email: signUpInputDto.email,
+    });
+    if (emailExists) {
+      throw Error('Email is already in use');
+    }
+    const password = await bcryptjs.hash(signUpInputDto.password, 10);
 
-//     const user = await this.prisma.client.createUser({
-//       ...signUpInputDto,
-//       password,
-//     });
+    const user = await this.prisma.client.createUser({
+      ...signUpInputDto,
+      password,
+    });
 
-//     const jwt = this.jwt.sign({ id: user.id });
-//     res.cookie('token', jwt, { httpOnly: true });
+    const jwt = this.jwt.sign({ id: user.id });
+    res.cookie('token', jwt, { httpOnly: true });
 
-//     return user;
-//   }
-// }
+    return user;
+  }
+}
